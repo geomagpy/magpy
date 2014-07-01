5d48c80a25d8d8f82e87ca9b6543c612c42d3a34
R Bailey <rachel.bailey@zamg.ac.at>
2014-07-01 10:53:44 +0000

    Small orrection

--- src/mpplot.py
+++ src/mpplot.py
@@ -1279,7 +1279,7 @@ def _plot(data,savedpi=80,grid=True,gridcolor='#316931',
             if fkey in function[0]:
 		# --> Get the minimum and maximum relative times
                 ttmp = arange(0,1,0.0001)
-                ax.plot_date(_denormalize(ttmp,function[1],function[2]),function[0][fkey](ttmp),'r-')
+                ax.plot_date(denormalize(ttmp,function[1],function[2]),function[0][fkey](ttmp),'r-')
 
         # PLOT SHADED AND ANNOTATED STORM PHASES:
         if 'stormphases' in data[i]:
@@ -1443,24 +1443,6 @@ def _confinex(ax, tmax, tmin, timeunit):
     else:
         ax.get_xaxis().set_major_formatter(matplotlib.dates.DateFormatter('%Y'))
         timeunit = '[Year]'
-
-def nan_helper(y):
-    """
-    Helper to handle indices and logical indices of NaNs. 
-    Taken from eat (http://stackoverflow.com/questions/6518811/interpolate-nan-values-in-a-numpy-array)
-
-    Input:
-            - y, 1d numpy array with possible NaNs
-    Output:
-            - nans, logical indices of NaNs
-            - index, a function, with signature indices= index(logical_indices),
-              to convert logical indices of NaNs to 'equivalent' indices
-    Example:
-            >>> # linear interpolation of NaNs
-            >>> nans, x= nan_helper(y)
-            >>> y[nans]= np.interp(x(nans), x(~nans), y[~nans])
-    """
-    return np.isnan(y), lambda z: z.nonzero()[0]
 
 
 if __name__ == '__main__':
