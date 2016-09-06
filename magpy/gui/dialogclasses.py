b5882f557fe631d557b9e9895cacfd13d32da695
Mojca Miklavec <mojca.miklavec@viviss.si>
2016-09-06 14:37:54 +0200

    dialogclasses.py: enlarge the grid
    The grid currently has 16 elements, so we need a 8x2 rather than 4x2 grid.

--- magpy/gui/dialogclasses.py
+++ magpy/gui/dialogclasses.py
@@ -381,7 +381,7 @@ class ExportModifyNameDialog(wx.Dialog):
         # and the logger text control (on the right):
         boxSizer = wx.BoxSizer(orient=wx.HORIZONTAL)
         # A GridSizer will contain the other controls:
-        gridSizer = wx.FlexGridSizer(rows=4, cols=2, vgap=10, hgap=10)
+        gridSizer = wx.FlexGridSizer(rows=8, cols=2, vgap=10, hgap=10)
 
         # Prepare some reusable arguments for calling sizer.Add():
         expandOption = dict(flag=wx.EXPAND)
