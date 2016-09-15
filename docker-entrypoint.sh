2882a41ec527df7d7e596380e3b1a3ea74c1f745
Jeremy Fee <jmfee@usgs.gov>
2016-09-15 23:19:14 +0200

    Initial Dockerfile

---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# run jupyter in the background, and forward SIGTERM manually.
+# "exec" seems like a much simpler solution for this,
+# however, jupyter kernels die noisy deaths when using exec.
+
+_term () {
+  echo 'Caught SIGERM'
+  kill -TERM "$child"
+}
+trap _term SIGTERM
+
+
+# run jupyter notebook server
+jupyter notebook \
+    --ip='*' \
+    --notebook-dir=/home/magpy_user/notebooks \
+    --no-browser \
+    --port=8000 &
+
+child=$!
+wait "$child"
