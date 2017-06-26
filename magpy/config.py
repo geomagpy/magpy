4e473cfdbf14da5adee3950b892f4da660d9f938
Heather Schovanec <hschovanec@usgs.gov>
2017-06-26 08:11:02 -0600

    Updated default config file

--- magpy/config.py
+++ magpy/config.py
@@ -16,7 +16,7 @@ path_to_config = os.path.join(directory, config_file_name)
 if not os.path.exists(path_to_config):
     with open(path_to_config, "w") as cfg:
         data = {}
-        data['droppedValue'] = 22222
+        data['droppedValue'] = 99999
         json.dump(data, cfg, ensure_ascii=False)
     with open(path_to_config, "r") as cfg:
         data = json.load(cfg)
