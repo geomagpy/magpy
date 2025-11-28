e6c393746515ec8a755cd62d39a8a0ecac3688dc
leonro <roman.leonhardt@geosphere.at@>
2025-11-28 18:29:13 +0100

    corrected flag_range if above and below are given

--- magpy/core/flagging.py
+++ magpy/core/flagging.py
@@ -1723,7 +1723,7 @@ def flag_range(data, keys=None, above=0, below=0, starttime=None, endtime=None, 
 
     if above and below:
         ind = data.KEYLIST.index(keys[0])
-        trueindices = (trimmedstream.ndarray[ind] > above) & (trimmedstream.ndarray[ind] < below)
+        trueindices = (trimmedstream.ndarray[ind] <= above) & (trimmedstream.ndarray[ind] >= below)
         d = np.diff(trueindices)
         idx, = d.nonzero()
         idx += 1
